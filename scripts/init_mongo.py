c3a4f4a1f3d20674c5bb48b13d9715fb31d08645
Thomas <thomas.ripoll91@gmail.com>
2026-04-10 10:36:36 +0200

    Creation du docker et du fichier requirements.txt

---
+++ scripts/init_mongo.py
@@ -0,0 +1,14 @@
+{
+    "order_id": "c1",
+    "client": "Client A",
+    "driver_id": "d3",
+    "driver_name": "Charlie Lefevre",
+    "pickup_time": "2025-12-06T14:05:00Z",
+    "delivery_time": "2025-12-06T14:25:00Z",
+    "duration_minutes": 20,
+    "amount": 25,
+    "region": "Paris",
+    "rating": 4.9,
+    "review": "Excellent service !",
+    "status": "completed"
+}
